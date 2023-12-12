d06c74efb2d080bb4e9dada72877d57fda69ff41
Jhonata Williams <jhonatawwxx@gmail.com>
2023-12-11 21:06:09 -0300

    Feat: Teste para classe principal

--- src/test/java/com/example/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/example/springboot/SpringbootApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class SpringbootApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		SpringbootApplication.main(new String[] {});
 	}
 
 }
