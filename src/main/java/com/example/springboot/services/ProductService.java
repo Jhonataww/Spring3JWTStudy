245c4c624e2bf8a6d29b75ef99128a300af96a28
jhonata.brito <jhonata.brito@pdcase.com.br>
2023-12-18 10:06:52 -0300

    Fix: Correção do merge para as branchs.

--- src/main/java/com/example/springboot/services/ProductService.java
+++ src/main/java/com/example/springboot/services/ProductService.java
@@ -2,6 +2,8 @@ package com.example.springboot.services;
 
 import com.example.springboot.dtos.ProductForm;
 import com.example.springboot.models.ProductModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -12,4 +14,6 @@ public interface ProductService {
     public ArrayList<ProductModel> productFilter (String name, BigDecimal value);
 
     public ArrayList<ProductModel> findAllFilterGeneric(ProductForm filter);
+
+    public Page<ProductModel> productFilterPage(String name, BigDecimal value, Pageable pageable);
 }
